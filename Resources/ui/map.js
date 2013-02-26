e63b84d9a9e8b287579b2eb8a70976988997f355
Blackimp <j.neuberth@gmail.com>
2013-02-26 19:47:08 +0100

    map view implemented

--- Resources/ui/map.js
+++ Resources/ui/map.js
@@ -1,22 +1,58 @@
 /**
  * MAP  CONTENT
  */
-
 var win = Titanium.UI.currentWindow;
 
-var mapView = Titanium.Map.createView({
-	mapType: Titanium.Map.STANDARD_TYPE,
-	animate: true,
-	regionFit: true,
-	userLocation: true,
-	// Titanium.Map.STANDARD_TYPE
-	// Titanium.Map.HYBRID_TYPE
-	region:{
-		latitude:56.879945,
-		longitude:14.799449,
-		latitudeDelta:1,
-		longitudeDelta:1
+Titanium.Geolocation.accuracy = Titanium.Geolocation.ACCURACY_BEST;
+
+//
+//  SET DISTANCE FILTER.  THIS DICTATES HOW OFTEN AN EVENT FIRES BASED ON THE DISTANCE THE DEVICE MOVES
+//  THIS VALUE IS IN METERS
+//
+Titanium.Geolocation.distanceFilter = 10;
+
+//
+// GET CURRENT POSITION - THIS FIRES ONCE
+//
+Titanium.Geolocation.getCurrentPosition(function(e) {
+	if (e.error) {
+		alert('HFL cannot get your current location');
+		return;
 	}
+
+	var longitude = e.coords.longitude;
+	var latitude = e.coords.latitude;
+	var altitude = e.coords.altitude;
+	var heading = e.coords.heading;
+	var accuracy = e.coords.accuracy;
+	var speed = e.coords.speed;
+	var timestamp = e.coords.timestamp;
+	var altitudeAccuracy = e.coords.altitudeAccuracy;
+
+	// CREATE ANNOTATION
+
+/*	var annotation = Titanium.Map.createAnnotation({
+		latitude : latitude,
+		longitude : longiude,
+		title : "Your current location",
+		animate : true
+	})
+*/
+	
+	//
+	//CREATE MAP VIEW
+	//
+	var mapview = Titanium.Map.createView({
+	mapType : Titanium.Map.STANDARD_TYPE,
+	region : {
+	latitude : latitude,
+	longitude : longitude,
+	latitudeDelta : 0.01,
+	longitudeDelta : 0.01
+	},
+	animate : true,
+	regionFit : true,
+	userLocation : true
 });
 
-win.add(mapView);
+win.add(mapview);
