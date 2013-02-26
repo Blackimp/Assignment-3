25dbff325c49fbc504ecdb92d242157c48e6da2f
unknown <plexus@.(none)>
2013-02-26 15:36:19 +0100

    first real content to map and photo

--- Resources/ui/map.js
+++ Resources/ui/map.js
@@ -4,8 +4,19 @@
 
 var win = Titanium.UI.currentWindow;
 
-var label = Titanium.UI.createLabel({
-	text: "Map"
+var map = Titanium.Map.createView({
+	mapType: Titanium.Map.SATELLITE_TYPE,
+	animate: true,
+	regionFit: true,
+	userLocation: true,
+	// Titanium.Map.STANDARD_TYPE
+	// Titanium.Map.HYBRID_TYPE
+	region:{
+		latitude:43.85514,
+		longitude:-1.385032,
+		latitudeDelta:1,
+		longitudeDelta:1
+	}
 });
 
-win.add(label);
+win.add(map);
--- Resources/ui/photo.js
+++ Resources/ui/photo.js
@@ -4,8 +4,30 @@
 
 var win = Titanium.UI.currentWindow;
 
-var label = Titanium.UI.createLabel({
-	text: "Photo"
+var btn = Titanium.UI.createButton({
+	title: "Make a Photo",
+	bottom: 20
 });
 
-win.add(label);
+btn.addEventListener("click", function(e){
+	Titanium.Media.showCamera({
+		saveToPhotoGallery: false,
+		mediaTypes: [Titanium.Media.MEDIA_TYPE_PHOTO],
+		success:function(e){
+			var img = Titanium.UI.createImageView({
+				image:e.media,
+				top: 20
+			})
+			
+			win.add(img);
+		},
+		error:function(e){
+			alert("Something went terrible wrong.");
+		},
+		cancel:function(e){
+			alert("Make up your damn mind.");
+		},
+	})
+})
+
+win.add(btn);
