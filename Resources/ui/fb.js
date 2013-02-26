988fba44467a8d2c69eeab8815ee01516619fd60
Blackimp <j.neuberth@gmail.com>
2013-02-27 00:01:20 +0100

    Facebook Login implemented

--- Resources/ui/fb.js
+++ Resources/ui/fb.js
@@ -4,8 +4,49 @@
 
 var win = Titanium.UI.currentWindow;
 
-var label = Titanium.UI.createLabel({
-	text: "Facebook"
+Ti.Facebook.appid = '442274525852593';
+Ti.Facebook.permissions = ['publish_stream'];
+Ti.Facebook.addEventListener('login', function(e) {
+    if (e.success) {
+        alert('Logged in');
+    }
 });
+Ti.Facebook.addEventListener('logout', function(e) {
+    alert('Logged out');
+});
+    
+// Add the button.  Note that it doesn't need a click event listener.
+win.add(Ti.Facebook.createLoginButton({
+    top : 50,
+    style : Ti.Facebook.BUTTON_STYLE_WIDE
+}));
 
-win.add(label);
+
+/*
+ * 
+ * Post a Photo
+/// First make sure this permission exists
+Ti.Facebook.permissions = ['publish_stream'];
+Ti.Facebook.authorize();
+
+// ...
+// ...
+
+// Now post the photo after you've confirmed that authorize() succeeded
+var f = Ti.Filesystem.getFile('pumpkin.jpg');
+var blob = f.read();
+var data = {
+    message: 'This is a pumpkin',
+    picture: blob
+};
+Ti.Facebook.requestWithGraphPath('me/photos', data, 'POST', function(e){
+    if (e.success) {
+        alert("Success!  From FB: " + e.result);
+    } else {
+        if (e.error) {
+            alert(e.error);
+        } else {
+            alert("Unkown result");
+        }
+    }
+});*/
