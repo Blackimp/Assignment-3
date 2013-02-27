76c1d0d0600adc2890acfa8790949cea11ff9c1a
Blackimp <j.neuberth@gmail.com>
2013-02-27 01:13:39 +0100

    Facebook Photo Upload implemented

--- Resources/ui/photo.js
+++ Resources/ui/photo.js
@@ -5,29 +5,55 @@
 var win = Titanium.UI.currentWindow;
 
 var btn = Titanium.UI.createButton({
-	title: "Make a Photo",
-	bottom: 20
+	title : "Make a Photo",
+	bottom : 20
 });
 
-btn.addEventListener("click", function(e){
+btn.addEventListener("click", function(e) {
 	Titanium.Media.showCamera({
-		saveToPhotoGallery: false,
-		mediaTypes: [Titanium.Media.MEDIA_TYPE_PHOTO],
-		allowEditing: false,
-		success:function(e){
+		saveToPhotoGallery : false,
+		mediaTypes : [Titanium.Media.MEDIA_TYPE_PHOTO],
+		allowEditing : false,
+		success : function(e) {
 			alert("Photo taken.");
-			
+
 			var img = Titanium.UI.createImageView({
-				image:e.media,
-				top: 20
+				image : e.media,
+				top : 20
 			})
-			
 			win.add(img);
+			
+			var image = e.media;
+			var f = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, 'camera_photo.png');
+			f.write(image);
+			//win.backgroundImage = f.nativePath;
+			
+			Ti.Facebook.permissions = ['publish_stream'];
+			Ti.Facebook.authorize();
+
+			// Now post the photo after you've confirmed that authorize() succeeded
+			// var f = Ti.Filesystem.getFile(img);
+			var blob = f.read();
+			var data = {
+				message : 'Titanium Mobile Test',
+				picture : blob
+			};
+			Ti.Facebook.requestWithGraphPath('me/photos', data, 'POST', function(e) {
+				if (e.success) {
+					alert("Success!  From FB: " + e.result);
+				} else {
+					if (e.error) {
+						alert(e.error);
+					} else {
+						alert("Unkown result");
+					}
+				}
+			});
 		},
-		error:function(e){
+		error : function(e) {
 			alert("Something went terrible wrong.");
 		},
-		cancel:function(e){
+		cancel : function(e) {
 			alert("Make up your damn mind.");
 		}
 	})
