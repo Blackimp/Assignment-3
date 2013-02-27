d51636434264993fe70a68ba07af150a1a420cf3
Blackimp <j.neuberth@gmail.com>
2013-02-27 10:42:53 +0100

    deleted some comments

--- Resources/ui/fb.js
+++ Resources/ui/fb.js
@@ -20,33 +20,3 @@ win.add(Ti.Facebook.createLoginButton({
     top : 50,
     style : Ti.Facebook.BUTTON_STYLE_WIDE
 }));
-
-
-/*
- * 
- * Post a Photo
-/// First make sure this permission exists
-Ti.Facebook.permissions = ['publish_stream'];
-Ti.Facebook.authorize();
-
-// ...
-// ...
-
-// Now post the photo after you've confirmed that authorize() succeeded
-var f = Ti.Filesystem.getFile('pumpkin.jpg');
-var blob = f.read();
-var data = {
-    message: 'This is a pumpkin',
-    picture: blob
-};
-Ti.Facebook.requestWithGraphPath('me/photos', data, 'POST', function(e){
-    if (e.success) {
-        alert("Success!  From FB: " + e.result);
-    } else {
-        if (e.error) {
-            alert(e.error);
-        } else {
-            alert("Unkown result");
-        }
-    }
-});*/
--- Resources/ui/map.js
+++ Resources/ui/map.js
@@ -58,22 +58,3 @@ Titanium.Geolocation.getCurrentPosition(function(e) {
 	win.add(mapview);
 
 });
-
-/*var win = Titanium.UI.currentWindow;
-
- var mapView = Titanium.Map.createView({
- mapType: Titanium.Map.STANDARD_TYPE,
- animate: true,
- regionFit: true,
- userLocation: true,
- // Titanium.Map.STANDARD_TYPE
- // Titanium.Map.HYBRID_TYPE
- region:{
- latitude:56.879945,
- longitude:14.799449,
- latitudeDelta:1,
- longitudeDelta:1
- }
- });
-
- win.add(mapView);*/
