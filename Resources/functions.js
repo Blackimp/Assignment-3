aca019d6ba81bcb5497a60c9346288712e940d96
Blackimp <j.neuberth@gmail.com>
2013-02-28 01:14:57 +0100

    yfd

--- Resources/functions.js
+++ Resources/functions.js
@@ -3,7 +3,37 @@
  */
 
 function getPosition() {
+	Titanium.Geolocation.getCurrentPosition(function(e) {
+		if (e.error) {
+			alert('HFL cannot get your current location');
+			return;
+		}
 
+		var longitude = e.coords.longitude;
+		var latitude = e.coords.latitude;
+		var altitude = e.coords.altitude;
+		var heading = e.coords.heading;
+		var accuracy = e.coords.accuracy;
+		var speed = e.coords.speed;
+		var timestamp = e.coords.timestamp;
+		var altitudeAccuracy = e.coords.altitudeAccuracy;
+	})
+}
+
+function storeJSON(image) {
+	var encodedImg = Titanium.Utils.base64encode(image.toString);
+	if (latitude != 0 && longitude != 0) {
+		var dictData = {
+			img : encodedImg,
+			latitude : latitude,
+			longitude : longitude
+		}
+	} else {
+		var dictData = {
+			img : encodedImg
+		}
+	}
+	var jsonData = JSON.stringify();
 }
 
 // For loading img and location
@@ -11,7 +41,7 @@ function loadJSON() {
 	// TODO: write last img to class var 'img'
 }
 
-function publishPhoto(){
-	
+function publishPhoto() {
+
 }
 
